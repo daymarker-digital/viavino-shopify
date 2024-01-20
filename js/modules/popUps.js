12d54bbedb0440eb0f17a95c259022e6a600e21e
havemersey <brenden@daymarker.digital>
2024-01-20 11:04:27 -0800

    First pass on mobile styles and colour-theme settings for new section.

--- js/modules/popUps.js
+++ js/modules/popUps.js
@@ -10,7 +10,8 @@ class PopUps {
 
   constructor( block=false ) {
     this.DOM = {
-      block,
+      block: block,
+      button_close: block.querySelector(".js--close-pop-up")
     };
     this.modal = {
       id: block.id,
@@ -21,12 +22,33 @@ class PopUps {
     this.start();
   }
 
+  handleCloseClick() {
+    if ( this.DOM.button_close && this.modal.instance ) {
+      this.DOM.button_close.addEventListener("click", (e) => {
+        this.modal.instance.hide();
+        this.enableCookie();
+      })
+    }
+  }
+
+  enableCookie() {
+    console.log('cookie enabled!');
+  }
+
   start() {
     if ( true ) {
+
+      // init modal instance
       this.modal.instance = new bootstrap.Modal(this.DOM.block, {});
+      // show modal
       this.showPopUp();
+      // add event listener to close button
+      this.handleCloseClick();
+
+      // log data
       console.log(this.modal);
       console.log(this.modal.instance._isShown);
+
     }
   }
 
